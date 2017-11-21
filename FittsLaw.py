e4883dfd2f0b3cfb54068d906a305bbf6e77b98d
Viren Mody <VirenMody@users.noreply.github.com>
2017-11-20 23:29:35 -0600

    Added single iteration of Fitts's Law with Time

--- FittsLaw.py
+++ FittsLaw.py
@@ -1,7 +1,7 @@
 from graphics import *
 from graphicsexamples import *
 import math
-
+import time
 
 # checks where use clicked in target
 def clickedInTarget(clickedPoint, target):
@@ -13,50 +13,65 @@ def clickedInTarget(clickedPoint, target):
     return distance <= target.getRadius()
 
 def main():
-    xMax = yMax = 500
-    distance = 400
-    radius = distance/2
-    width = 50
+    windowSize = 700
+    xyMax = windowSize / 2 - 1
+    xyMin = -1 * xyMax
+
     numTargets = 20
+    targetWidth = 45
+    diameter = 200
+    radius = diameter/2
     degreesBetweenTargets = 360/numTargets
 
     # Create graphical window and center origin
-    win = GraphWin("Fitts' Law", xMax, yMax)
+    win = GraphWin("Fitts' Law", windowSize, windowSize)
     win.setBackground("white")
-    win.setCoords(-249, -249, 249, 249)
+    win.setCoords(xyMin, xyMin, xyMax, xyMax)
 
     # Center circle
     c = Circle(Point(0, 0), 10)
     c.setFill("blue")
     c.draw(win)
 
+
     circleList = []
 
-    # Draw circle of circles
+    # Populate circleList with circles of targetWidth and diameter/amplitude
     for i in range(0, numTargets):
         xCenter = radius*(math.cos(math.radians(degreesBetweenTargets*i)))
         yCenter = radius*(math.sin(math.radians(degreesBetweenTargets*i)))
 
         print ('Iteration: ', i,' --> ', xCenter, ', ', yCenter)
-        c = Circle(Point(xCenter, yCenter), width/2)
-        c.setOutline("gray")
+        c = Circle(Point(xCenter, yCenter), targetWidth/2)
+        c.setOutline("black")
+        c.setFill("gray")
         circleList.append(c)
 
-    startTarget = circleList[0]
-    endTarget = circleList[10]
-    startTarget.setFill("red")
-
+    # Draw circle of circles
     for circle in circleList:
         circle.draw(win)
 
-    clickedPoint = win.getMouse()
-    target = startTarget
-    if clickedInTarget(clickedPoint, target):
-        print('You did it!!!!!')
+    for i in range(0, int(numTargets/2)):
+        startTarget = circleList[i]
+        endTarget = circleList[i+int(numTargets/2)]
+        startTarget.setFill("red")
 
+        clickedPoint = win.getMouse()
+        if clickedInTarget(clickedPoint, startTarget):
+            startTime = time.time()
+            print('You got the START!!')
+            startTarget.setFill("gray")
+            endTarget.setFill("red")
+
+        clickedPoint = win.getMouse()
+        if clickedInTarget(clickedPoint, endTarget):
+            endTime = time.time()
+            print('You got the END!!!')
+            endTarget.setFill("gray")
+
+        print(endTime-startTime)
 
     #exit
-    win.getMouse() # pause for click in window
     win.close()
 
 
